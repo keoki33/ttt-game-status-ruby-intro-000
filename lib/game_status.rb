be4899bbb147b7ea5b890908d70a278938b1835d
George Wong <gwong323@yahoo.com>
2019-01-30 21:06:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,24 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]
+
+def won?(board)
+WIN_COMBINATIONS.each do |win_index|
+  if win_index == [X, X, X] || [O, O, O]
+    won?(board)
+else
+  false
+
+end
+end
+end
